19bf0234aad6e471625d8496217ea59a1309795d
Jan Richter <falnyr@gmail.com>
2021-06-16 07:39:22 +1200

    feat: embedded relations

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -52,7 +52,7 @@ class User implements UserInterface
     private $password;
 
     /**
-     * @Groups({"user:read", "user:write"})
+     * @Groups({"user:read", "user:write", "cheese_listing:item:get"})
      * @ORM\Column(type="string", length=255, unique=true)
      * @Assert\NotBlank()
      */
