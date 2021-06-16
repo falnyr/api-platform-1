de12b9af00f6e5b126976223fbb906be1156f1c7
Jan Richter <falnyr@gmail.com>
2021-06-16 17:16:04 +1200

    feat: creating nested objects

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -51,7 +51,7 @@ class CheeseListing
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"cheese_listing:read", "cheese_listing:write", "user:read"})
+     * @Groups({"cheese_listing:read", "cheese_listing:write", "user:read", "user:write"})
      * @Assert\NotBlank()
      * @Assert\Length(min=2,max=50,maxMessage="Describe your cheese in 50 chars or less")
      */
@@ -68,7 +68,7 @@ class CheeseListing
      * The price of our delicious cheese in cents.
      *
      * @ORM\Column(type="integer")
-     * @Groups({"cheese_listing:write", "user:read"})
+     * @Groups({"cheese_listing:write", "user:read", "user:write"})
      * @Assert\NotBlank()
      */
     private $price;
@@ -134,7 +134,7 @@ class CheeseListing
     /**
      * The description of the cheese as raw text.
      *
-     * @Groups({"cheese_listing:write"})
+     * @Groups({"cheese_listing:write", "user:write"})
      * @SerializedName("description")
      *
      * @param string $description
